8a78db3176bb3ef6b5c1314855406c14d15bcf77
Daniel <daniel_tian2002@hotmail.com>
2018-01-24 13:45:21 -0800

    Project initialization

---
+++ TowerVille/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TowerVille
+//
+//  Created by Daniel Tian on 2018-01-24.
+//  Copyright © 2018 The-Fighting-Mongeese. All rights reserved.
+//
+
+import UIKit
+import GLKit
+
+class ViewController: GLKViewController { //UIViewController
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
