ed55067f036d975c5307b382f74297cd523f3ca2
limbermuffin <limbermuffin@gmail.com>
2018-02-19 19:21:08 -0800

    Markus is an idiot who forgets to change coordinates in tiles

--- TowerVille/GameEngine/Tile.swift
+++ TowerVille/GameEngine/Tile.swift
@@ -13,16 +13,16 @@ class Tile : VisualObject {
     
     // TODO: needs to be changed, model data should always be centered.
     static let vertexData = [
-        VertexData( 0.9, 0, 0.9, 1, 0, 0, 1,  1, 0,  0, 1, 0),
-        VertexData( 0.9, 0, 0.1, 0, 1, 0, 1,  1, 1,  0, 1, 0),
-        VertexData( 0.1, 0, 0.1, 0, 0, 1, 1,  0, 1,  0, 1, 0),
-        VertexData( 0.1, 0, 0.9, 0, 0, 0, 1,  0, 0,  0, 1, 0),
+        VertexData( 0.9, 0, -0.9, 1, 0, 0, 1,  1, 0,  0, 1, 0),
+        VertexData( 0.9, 0, -0.1, 0, 1, 0, 1,  1, 1,  0, 1, 0),
+        VertexData( 0.1, 0, -0.1, 0, 0, 1, 1,  0, 1,  0, 1, 0),
+        VertexData( 0.1, 0, -0.9, 0, 0, 0, 1,  0, 0,  0, 1, 0),
     ]
 
     // Note: Order matters
     static let indexData : [GLubyte] = [
-        0,1,2,
-        2,3,0
+        2,1,0,
+        0,3,2
     ]
     
     var xCoord : uint = 0
