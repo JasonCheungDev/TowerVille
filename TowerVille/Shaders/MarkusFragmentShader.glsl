82759f23be003702a23c03b5a1398248e71b5e4d
limbermuffin <limbermuffin@gmail.com>
2018-02-21 12:49:46 -0800

    changed : Markus frag shader allow multiple lights changed : viewController use Markus frag changed : viewController only draw tiles on screen changed : light positions & colors changed : debug renderObject positions removed : Markus & Jason vert shader

--- TowerVille/Shaders/MarkusFragmentShader.glsl
+++ TowerVille/Shaders/MarkusFragmentShader.glsl
@@ -2,34 +2,62 @@
 
 precision mediump float;
 
-uniform sampler2D u_Texture;    // sampler3D also exists
+struct DirectionalLight {
+    mediump float intensity;
+    mediump vec3 direction;
+    mediump vec4 color;
+};
 
-in vec4 frag_Color;
-in vec2 frag_TexCoord;
-in vec3 frag_Normal;
-in vec3 frag_Position;
+struct PointLights {
+    mediump float intensity;
+    mediump vec3 position;
+    mediump vec4 color;
+};
 
-out vec4 o_color;
+uniform sampler2D u_Texture;
+uniform DirectionalLight u_DirectionalLight;
+uniform PointLights[4] u_PointLights;
+uniform highp mat4 u_ModelView;
 
-void main()
-{
-    lowp vec4 debugLightColor = vec4(1.0, 1.0, 1.0, 1.0);
-    vec3 debugLightPosition = vec3(7.07107, 4.08248, -14.4338); // (5,5,-5) worldspace
+in lowp vec4 frag_Color;
+in lowp vec2 frag_TexCoord;
+in lowp vec3 frag_Normal;
+in lowp vec3 frag_Position;
+
+out lowp vec4 o_color;
+
+void main() {
+    float attenuationCoef = 0.15;
+    float specularPower = 20.0;
     
     vec3 normal = normalize(frag_Normal);
-    vec3 lightDirection = debugLightPosition - frag_Position;
-    vec3 halfDirection = normalize(vec3(0.0, 0.0, 1.0)+normalize(lightDirection));
     
-    float halfLambert = pow(dot(normal, normalize(lightDirection)) * 0.5 + 0.5, 2.0);
-    float blinn = pow(max(0.0, dot(normal, halfDirection)), 10.0);
-    float attenuation = 1.0 / pow(length(lightDirection) * 0.2, 2.0);
+    vec3 halfDirection = normalize(vec3(0.0, 0.0, 1.0)+normalize(-u_DirectionalLight.direction));
+    float halfLambert = pow(dot(normal, normalize(-u_DirectionalLight.direction)) * 0.5 + 0.5, 2.0);
+    float blinn = pow(max(0.0, dot(normal, halfDirection)), specularPower);
     
-    vec4 linearColor;
-    if (textureSize(u_Texture, 0).x == 0) {
-        linearColor = debugLightColor * attenuation * (blinn + halfLambert * frag_Color);
-    } else {
-        linearColor = debugLightColor * attenuation * (blinn + halfLambert * frag_Color * texture(u_Texture, frag_TexCoord));
+    vec4 diffuse = u_DirectionalLight.color * u_DirectionalLight.intensity * halfLambert;
+    vec4 specular = u_DirectionalLight.color * u_DirectionalLight.intensity * blinn;
+    
+    for (int i = 0; i < 2; i++) {
+        vec3 lightDirection = u_PointLights[i].position - frag_Position;
+        vec3 halfDirection = normalize(vec3(0.0, 0.0, 1.0)+normalize(lightDirection));
+        
+        float halfLambert = pow(dot(frag_Normal, normalize(lightDirection)) * 0.5 + 0.5, 2.0);
+        float blinn = pow(max(0.0, dot(frag_Normal, halfDirection)), specularPower);
+        float attenuation = u_PointLights[i].intensity / pow(length(lightDirection) * attenuationCoef, 2.0);
+        
+        diffuse += u_PointLights[i].color * attenuation * halfLambert;
+        specular += u_PointLights[i].color * attenuation * blinn;
     }
     
+    if (textureSize(u_Texture, 0).x != 0) {
+        diffuse *= texture(u_Texture, frag_TexCoord);
+    }
+    
+    //TODO : remove
+    vec4 goat = mix(frag_Color, vec4(0.5,0.5,0.5,1.0), 0.33); // makes colors less saturated
+    
+    vec4 linearColor = diffuse * goat + specular;
     o_color = sqrt(linearColor);
 }
