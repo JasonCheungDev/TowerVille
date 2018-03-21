673bd9b8c3959e7abf9b4e32ed5a93c5ab05d741
limbermuffin <limbermuffin@gmail.com>
2018-03-21 15:19:36 -0700

    shader optimizations

--- TowerVille/Shaders/MarkusFragmentShader.glsl
+++ TowerVille/Shaders/MarkusFragmentShader.glsl
@@ -1,4 +1,4 @@
-#define M_PI 3.1415926535897932384626433832795
+#define INVERSE_8_PI 0.0397887357729738339422209408431
 
 precision mediump float;
 
@@ -29,32 +29,45 @@ varying lowp vec3 frag_Position;
 // out lowp vec4 o_color;
 
 void main() {
-    float attenuationCoef = 0.15;
+    const vec3 cameraForward = vec3(0.0, 0.0, 1.0);
+    const float attenuationCoef = 0.13;
+    
     vec3 normal = normalize(frag_Normal);
     
-    vec3 halfDirection = normalize(vec3(0.0, 0.0, 1.0)+normalize(-u_DirectionalLight.direction));
-    float halfLambert = pow(dot(normal, normalize(-u_DirectionalLight.direction)) * 0.5 + 0.5, 2.0);
-    float blinn = (2.0 + u_SpecularPower) * pow(max(0.0, dot(normal, halfDirection)), u_SpecularPower) / (8.0 * M_PI);
+    /*
+     vec3 halfDirection = normalize(vec3(0.0, 0.0, 1.0)+normalize(-u_DirectionalLight.direction));
+     float halfLambert = pow(dot(normal, normalize(-u_DirectionalLight.direction)) * 0.5 + 0.5, 2.0);
+     float blinn = (2.0 + u_SpecularPower) * pow(max(0.0, dot(normal, halfDirection)), u_SpecularPower) / (8.0 * M_PI);
+     
+     vec4 diffuse = u_DirectionalLight.color * u_DirectionalLight.intensity * halfLambert;
+     vec4 specular = u_DirectionalLight.color * u_DirectionalLight.intensity * blinn;
+     */
     
-    vec4 diffuse = u_DirectionalLight.color * u_DirectionalLight.intensity * halfLambert;
-    vec4 specular = u_DirectionalLight.color * u_DirectionalLight.intensity * blinn;
+    vec4 diffuse = vec4(0.0);
+    vec4 specular = vec4(0.0);
     
     for (int i = 0; i < 2; i++) {
         vec3 lightDirection = u_PointLights[i].position - frag_Position;
-        vec3 halfDirection = normalize(vec3(0.0, 0.0, 1.0)+normalize(lightDirection));
+        float lightDistance = length(lightDirection);
+        lightDirection /= lightDistance;
         
-        float halfLambert = pow(dot(normal, normalize(lightDirection)) * 0.5 + 0.5, 2.0);
-        float blinn = (2.0 + u_SpecularPower) * pow(max(0.0, dot(normal, halfDirection)), u_SpecularPower) / (8.0 * M_PI);
-        float attenuation = u_PointLights[i].intensity / pow(length(lightDirection) * attenuationCoef, 2.0);
+        float halfLambert = dot(normal, lightDirection) * 0.5 + 0.5;
         
-        diffuse += u_PointLights[i].color * attenuation * halfLambert;
+        vec3 halfDirection = normalize(cameraForward+lightDirection);
+        float blinn = (2.0 + u_SpecularPower) * pow(max(0.0, dot(normal, halfDirection)), u_SpecularPower) * INVERSE_8_PI;
+        
+        float attenuation = 1.0 / (lightDistance * attenuationCoef);
+        attenuation *= attenuation * u_PointLights[i].intensity;
+        
+        diffuse += u_PointLights[i].color * attenuation * halfLambert * halfLambert;
         specular += u_PointLights[i].color * attenuation * blinn;
     }
     
     if (u_HasTexture) {
-        diffuse *= pow(texture2D(u_Texture, frag_TexCoord), vec4(2.2/1.0));
+        diffuse *= texture2D(u_Texture, frag_TexCoord) * texture2D(u_Texture, frag_TexCoord);
     }
     
     vec4 linearColor = diffuse * frag_Color + specular;
-    gl_FragColor = pow(linearColor, vec4(1.0/2.2));
+    gl_FragColor = sqrt(linearColor);
 }
+
